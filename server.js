df85f8cbe45768989099a6cafe00d19ba9452267
Shivamvishwakar <99338147+Shivamvishwakar@users.noreply.github.com>
2023-02-02 06:39:43 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(__dirname + '/client'));
+//Setup Environment variable
+const port = process.env.PORT || 3000;
+
+app.listen(port, ()=>{
+	console.log('Server is running on the  port ' + port);
+});
+
+
+
+
