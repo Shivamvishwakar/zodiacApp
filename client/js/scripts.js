e4fda3f98658cd1fd8bf34ce5e690848320234a4
Shivamvishwakar <99338147+Shivamvishwakar@users.noreply.github.com>
2023-02-23 07:09:11 +0000

    few changes made

--- client/js/scripts.js
+++ client/js/scripts.js
@@ -1,3 +1,78 @@
+var zodiacs = [
+{
+	name: "DAILYHOROSCOPE",
+	month: "March 21-April 20",
+	description: "The DailyHoroscope is a popular app that's simple to use. You can access your daily, weekly and monthly horoscope, your zodiac sign's characteristics and compatibility with other signs and your Chinese horoscope for the current and previous year. The app also lets you customize it by toying with different options for colors and font size.",
+},
 
+{
+	name:"Time",
+	month:"April 20-May 20",
+	description:"This app functions as a virtual astrology teacher. You can use Time Nomad to track the placement of planets, minor bodies and asteroids in real-time and analyze birth charts for you or your friends. It also sends you notifications of upcoming astrological events. For those that have trouble reading the information, the app also sends out weekly astrology lessons and predictions.",
+},
+
+{
+	name:"STAR ",
+	month:"May 21-Jun 20",
+	description:"Using data gathered from NASA and professional astrologers, the Co-Star app is able to provide you with really personalized horoscopes based on your natal chart. If you want to read the data yourself, you can also track real-time movement of the planets and stars. The Co-Star app also has a feature that allows you to keep track of your friends' or lovers' horoscopes to see if you're really meant for each other.",
+},
+
+{
+	name:"ITUNES",
+	month:"Jun  21-July  20",
+	description:"You'll love how easy it is to navigate this horoscope app. The app gives you free daily horoscopes full of fun, colorful images. The app also provides you with a daily mood, lucky number, a color and a lucky time of the day. You can also use it to discover other information about your sign like its element, symbol and planet. The app has a fun feature that lets you share any horoscope with your friends.",
+},
+
+{
+	name:"iHoroscope ",
+	month:"July 21-Aug 20",
+	description:"With this app, you can read your horoscope for different time frames including your day, week or year. From the app, you can chat with experienced psychics who'll give more deeper and more personalized information than your horoscope and there are some extra features for purchase.",
+},
+
+{
+	name:"Susan",
+	month:"Aug 21- Sep 20",
+	description:"Astrology Zone is a daily horoscope app where you can read your daily and monthly horoscopes, check your compatibility with someone and read essays from astrologer Susan Miller. The app offers both a free version and a paid premium version, which gives you access to extra perks like a look at the week ahead, the option to look at today's, yesterday's and tomorrow's horoscopes and more access to Susan's essays on topics like Mercury retrograde.",
+},
+
+{
+	name:"TimePassages",
+	month:"Sep 21- Oct 20",
+	description:"On this app, you can also get an image of your birth chart while checking out the meanings of your sun, moon and rising sign. It also helps you keep track of moon phases and upcoming Mercury retrograde phases. It's sharing feature allows you to compare charts with your friends, so everyone in the know of upcoming events affecting your signs.",
+},
+
+
+
+{
+	name:"Chaturanga ",
+	month:"Oct 21- Nov 20",
+	description:"The Chaturanga Astrology app is based on the Vedic zodiac system. Using information about when you were born, the app is able to provide you with detailed and personalized answers to your most pressing questions. You can ask it anything, but some examples of questions are "Should I move to another country" and "What kind of partner do I need?"",
+},
+
+
+{
+	name:"AstroStyle ",
+	month:"Nov 21- December 20",
+	description:"The AstroStyle app puts the professional astrology reading of the AstroTwins, Tali and Ophira Edut, right in your pocket. The AstroTwins have done readings for celebrities including Beyoncé and Sting. On the app, you can find your daily, weekly and monthly horoscopes and find out whether your bae is into you by using their Love Horoscopes feature.",
+},
+
+{
+	name:"DAILY ",
+	month:"December 22- Jan 20",
+	description:"Aside with providing you with daily, monthly and yearly readings, Daily Horoscope Plus also has a celebrity match feature where you can check the compatibility between you and your celeb crush. The app also includes other features like tarot readings, numerology and your blood type personality.",
+},
+
+{
+	name:"Sanctuary ",
+	month:"Jan 21- Feb 15",
+	description:"Sanctuary will quickly become your astro BFF. From 'daily scopes' and lucky-charm emojis specific to your sign to fun astrology facts you can drop in conversations, this app will be your go-to spot for all things astrology. You can also find one-on-one readings with professional astrologers.",
+},
+{
+	name:"Chani ",
+	month:"Feb 21- March 20",
+	description:"If you are looking for insightful horoscopes and birth chart analysis, astrologer Chani Nicholas's app is the one for you. You will also find workshops, daily meditations, and even altar suggestions.",
+}
+
+]
 
 
